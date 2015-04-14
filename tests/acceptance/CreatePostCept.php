d48d884637c1041b6ce8754cd673fb77734de09f
Jace Ju <jaceju@gmail.com>
2015-04-14 10:45:19 +0800

    Create frist acceptance test.

---
+++ tests/acceptance/CreatePostCept.php
@@ -0,0 +1,15 @@
+<?php
+$I = new AcceptanceTester($scenario);
+$I->wantTo('create post');
+$I->amOnPage('/');
+
+$rand = rand(1, 9999);
+$nickname = 'Nickname ' . $rand;
+$message  = 'Message ' . $rand;
+
+$I->fillField("//input[@name='nickname']", $nickname);
+$I->fillField(['name' => 'message'], $message);
+$I->click('button[type=submit]');
+
+$I->seeCurrentUrlEquals('/');
+$I->see($message);
